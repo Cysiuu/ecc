360c8010e4341d96fe14f2526ea05716bb83a939
Krzysztof Kozyra <cysiuu021@protonmail.com>
2024-11-08 12:45:26 +0100

    Install vuetify library for vue ui

--- ecc-ui/src/App.vue
+++ ecc-ui/src/App.vue
@@ -1,12 +1,20 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
   <div id="app">
-    <main>
-    </main>
+    <v-app>
+      <v-main>
+        <main>
+          <v-container>
+            <v-btn color="primary">Vuetify is working!</v-btn>
+          </v-container>
+        </main>
+      </v-main>
+    </v-app>
   </div>
-  <RouterView />
 </template>
 
+<style>
+</style>
--- ecc-ui/src/main.js
+++ ecc-ui/src/main.js
@@ -1,11 +1,26 @@
+import { createApp } from 'vue';
 
-import { createApp } from 'vue'
+import router from './router';
+import axios from 'axios';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
-import router from './router'
-import axios from "axios";
 
-const app = createApp(App)
+import 'vuetify/styles';
 
-app.use(router)
-app.config.globalProperties.$axios = axios
-app.mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+  const app = createApp(App)
+  app.config.globalProperties.$axios = axios;
+  app.use(vuetify)
+  app.use(router)
+  app.mount('#app')
+
+
+
+
