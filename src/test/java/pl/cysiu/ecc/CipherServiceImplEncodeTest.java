a740709630127224bf6f723c1ced39c9309d9c9d
Krzysztof Kozyra <cysiuu021@protonmail.com>
2024-11-10 20:48:36 +0100

    Add more test to check if Cipher is correct

--- src/test/java/pl/cysiu/ecc/CipherServiceImplEncodeTest.java
+++ src/test/java/pl/cysiu/ecc/CipherServiceImplEncodeTest.java
@@ -17,7 +17,7 @@ public class CipherServiceImplEncodeTest {
     }
 
     @Test
-    public void testEncode_simpleShift() {
+    public void testApplyCipher_simpleShift() {
 
         String textToEncode = "abcde";
         String pattern = "1:3/2"; // Shift letters from position 1 to 3 by 2 positions
@@ -25,34 +25,58 @@ public class CipherServiceImplEncodeTest {
 
         String expected = "cdede";  // Expected result after shifting
 
-        String result = cipherService.encode(cm);
+        String result = cipherService.applyCipher(cm);
 
         assertEquals(expected, result, "The encoded result should match the expected output.");
     }
 
     @Test
-    public void testEncode_endPattern() {
+    public void testApplyCipher_endPattern() {
         String textToEncode = "cipher";
         String pattern = "3:e/4";  // Shift letters from position 3 to end by 4 positions
         CipherMessage cm = new CipherMessage(textToEncode, pattern);
 
         String expected = "citliv";  // Expected result after shifting
 
-        String result = cipherService.encode(cm);
+        String result = cipherService.applyCipher(cm);
 
         assertEquals(expected, result, "The encoded result should match the expected output.");
     }
 
     @Test
-    public void testEncode_noShift() {
+    public void testApplyCipher_noShift() {
         String textToEncode = "hello";
         String pattern = "1:5/0";  // No shift
         String expected = "hello";  // Expected result after no shift
         CipherMessage cm = new CipherMessage(textToEncode, pattern);
 
-        String result = cipherService.encode(cm);
+        String result = cipherService.applyCipher(cm);
 
         assertEquals(expected, result, "The encoded result should match the original text.");
+    }
+
+    @Test
+    public void testApplyCipher_singleCharShift(){
+        String textToEncode = "ab";
+        String pattern = "1:1/1;";  // Shift the only character by 1 position
+        String expected = "bc";
+        CipherMessage cm = new CipherMessage(textToEncode, pattern);
+
+        String result = cipherService.applyCipher(cm);
+
+        assertEquals(expected, result, "The encoded result should match the expected output.");
+    }
+
+    @Test
+    public void testApplyCipher_multiplePatterns() {
+        String textToEncode = "aaaaa";
+        String pattern = "1:2/4;3:4/5;5:5/1";
+        String expected = "eeffb";
+        CipherMessage cm = new CipherMessage(textToEncode, pattern);
+
+        String result = cipherService.applyCipher(cm);
+
+        assertEquals(expected, result, "The encoded result should match the expected output.");
     }
 
 }
